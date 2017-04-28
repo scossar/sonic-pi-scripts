af42bf33464e015d2d5c8dd631bdecc0ea8fe25c
scossar <scossar3@gmail.com>
2017-04-28 01:11:07 -0700

    Update file

--- algo_rave.rb
+++ algo_rave.rb
@@ -26,10 +26,10 @@ live_loop :drums do
   n =  pat_two.ring[i]
   sample :bd_fat, amp: 3 if i % 4 === 0
   with_fx :echo, phase: 0.5, mix: 0.125 do
-    sample :drum_snare_hard, amp: 2 if n === pat_two[0]
+    # sample :drum_snare_hard, amp: 2 if n === pat_two[0]
   end
-  sample :drum_cymbal_open, amp: 1.4 if n === pat_two[1]
-  sample :drum_cymbal_closed, amp: 2 if n === pat_two[2]
+  # sample :drum_cymbal_open, amp: 1.4 if n === pat_two[1]
+  # sample :drum_cymbal_closed, amp: 2 if n === pat_two[2]
   sample :tabla_tas2, amp: 1.4 if n === pat_two[3]
   sleep 0.5
 end
@@ -39,9 +39,9 @@ live_loop :frac do
   i = tick
   n = pat_one.ring[i]
   use_synth :pulse
-  uncomment do
-    play n - 24, pulse_width: 0.2, attack: 0.01, decay: 0.4, sustain_level: 0, amp: 1.2
-    play n - 12, pulse_width: 0.2, attack: 0.02, decay: 0.4, sustain_level: 0, amp: 1.2
+ uncomment do
+    # play n - 24, pulse_width: 0.2, attack: 0.01, decay: 0.4, sustain_level: 0, amp: 1.2
+    # play n - 12, pulse_width: 0.2, attack: 0.02, decay: 0.4, sustain_level: 0, amp: 1.2
      uncomment do
       with_fx :rlpf, cutoff: filt_pat.ring[i], res: n === pat_one[2] ? 0.98 : 0.5 do
         play n + 19, pulse_width: 0.2, attack: 0, dcay: 0.4, sustain_level: 0, amp: 0.6
@@ -55,9 +55,9 @@ end
 live_loop :frac_lead do
   sync :drums
   i = tick
-  n = chord_pat.ring[i]
+  n = chord_pat.ring[i - 1]
   use_synth :pulse
   with_fx :rlpf, cutoff: 70, res: 0.7 do
-    play_chord n, pulse_width: 0.3, attack:16, decay: 16, sustain_level: 0, amp: 0.7 if i % 16 === 7
+    play_chord n, pulse_width: 0.3, attack:16, decay: 16, sustain_level: 0, amp: 0.7 if i % 16 === 5
   end
 end
