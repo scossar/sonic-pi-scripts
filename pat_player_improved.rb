09955aa35a8b890d8b33a01459b819f65655be56
scossar <scossar3@gmail.com>
2017-05-18 21:12:03 -0700

    Improve pattern

--- pat_player_improved.rb
+++ pat_player_improved.rb
@@ -74,8 +74,8 @@ use_bpm 134
 
 live_loop :drum_pat_test do
   i = tick
-  pat_player 'c---|'                          , i, {rate: 1, amp: 0.6 + rand(0.2), remove_pipes: 1}
-  pat_player 'B-------|b-----B-'                  , i, {amp: 1.4, remove_pipes: 1}
+  pat_player '----x---|---xX---|----x---|---xX---', i, {rate: 1, amp: 3 + rand(0.2), remove_pipes: 1}
+  pat_player 'B-------|b-----B-', i, {amp: 1.4, remove_pipes: 1}
   pat_player '--s-----|--s-S--s|--s----S|--s-s--S', i, {amp: 0.9, remove_pipes: 1}
   pat_player 'l---llLl|l---l---|l---l---|l---lcCc', i, {amp: 0.7, remove_pipes: 1}
   sleep 0.25
